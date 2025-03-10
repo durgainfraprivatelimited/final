ee3afb971a2705b0d7ce097012ed39db7df45332
durgainfraprivatelimited <durgainfraprivatelimited@gmail.com>
2025-03-10 16:09:18 +0530

    Email and PhoneUpdate contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -189,13 +189,15 @@ const Contact = () => {
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
-                  <p>durgainfraprivatelimited@gmail.com</p>
+                  <p>shrivagdeviconstructions@gmail.com</p>
                 </div>
                 <div className="flex items-center space-x-4">
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <p>+91 7483445291</p>
+                  <p>+91 9741406259</p>
+                  <p>+91 8971552132</p>
+                  <p>+91 7624896303</p>
                 </div>
               </div>
 
